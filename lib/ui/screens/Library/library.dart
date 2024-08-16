ae6fc04d34bb9388c07a607e63145cca5d3ace81
Gambito <mavaldiviezo.abx@gmail.com>
2024-08-16 16:33:23 -0500

    nueva implementacion

--- lib/ui/screens/Library/library.dart
+++ lib/ui/screens/Library/library.dart
@@ -116,8 +116,8 @@ class PlaylistNAlbumLibraryWidget extends StatelessWidget {
         children: [
           Padding(
             padding: const EdgeInsets.only(left: 5.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 isBottomNavActive
                     ? const SizedBox(
